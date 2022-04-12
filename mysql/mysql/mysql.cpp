682ffe70c920151e2d45076834efe907926adbf0
JCanizalesV <100484683+JCanizalesV@users.noreply.github.com>
2022-04-11 23:44:27 -0600

    ARCHIVOS PARA CONECTAR MYSQL CON C++
    se suben los archivos de la tarea

---
+++ mysql/mysql/mysql.cpp
@@ -0,0 +1,24 @@
+
+#include <mysql.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    MYSQL* conectar;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "123456", "db_empresa", 3306, NULL, 0);
+
+    if (conectar) {
+        cout << "LA CONEXION SE REALIZO CON EXITO" << endl;
+    }
+    else {
+        cout << "NO SE PUDO REALIZAR LA CONEXION" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
+
